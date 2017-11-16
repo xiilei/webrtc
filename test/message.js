cbaf20561dc31f1b631fe82ae93e47d25e2b583e
xiilei <xilei125@163.com>
2017-11-16 10:56:26 +0800

    添加encodeType,decodeType,解析消息类型,删除messageType

--- test/message.js
+++ test/message.js
@@ -1,14 +1,21 @@
-const { messageType, MethodBinding,
- ClassRequest, ClassSuccessResponse, ClassErrorResponse } = require('../lib/ice/stun/message');
+const { encodeType, decodeType, MethodBinding,
+  ClassRequest, ClassSuccessResponse, ClassErrorResponse } = require('../lib/ice/stun/message');
 const assert = require('power-assert');
 
 
 describe('#message', function () {
- it('messageType', function () {
-  assert.equal(messageType(MethodBinding, ClassRequest), 0x0001);
-  assert.equal(messageType(MethodBinding, ClassSuccessResponse), 0x0101);
-  assert.equal(messageType(MethodBinding, ClassErrorResponse), 0x0111);
-  assert.equal(messageType(0xb6d, 0x3), 0x2ddd);
- });
+  it('encodeType', function () {
+    assert.equal(encodeType(MethodBinding, ClassRequest), 0x0001);
+    assert.equal(encodeType(MethodBinding, ClassSuccessResponse), 0x0101);
+    assert.equal(encodeType(MethodBinding, ClassErrorResponse), 0x0111);
+    assert.equal(encodeType(0xb6d, 0x3), 0x2ddd);
+  });
+
+  it('decodeType', function () {
+    assert.deepEqual(decodeType(0x0001), { m: MethodBinding, c: ClassRequest });
+    assert.deepEqual(decodeType(0x0101), { m: MethodBinding, c: ClassSuccessResponse });
+    assert.deepEqual(decodeType(0x0111), { m: MethodBinding, c: ClassErrorResponse });
+    assert.deepEqual(decodeType(0x2ddd), { m: 0xb6d, c: 0x3 });
+  });
 });
 
